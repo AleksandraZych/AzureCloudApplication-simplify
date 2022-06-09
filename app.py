190a99bdf6b5aab659fe7b37f828cfd7a406cfac
Azych <45856462+Azych@users.noreply.github.com>
2022-06-09 18:42:41 +0200

    add mock and items

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
-
+from mock import *
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
 	""" returns rendered homepage """
 	# items = db_helper.fetch_todo()
+	items = all_mock
 	# files = os.listdir("app/uploads")
-	#  items=items,, files=files
-	return render_template("index.html")
+	#  ,, files=files
+	return render_template("index.html", items=items)
+
+
 if __name__ == '__main__':
 	app.run()
---
+++ mock.py
@@ -0,0 +1,13 @@
+all_mock = [
+    {
+        'id': '1',
+        'task': 'Zdać',
+        'status': 'In progress'
+    },
+    {
+        'id': '2',
+        'task': 'Chlać',
+        'status': 'In progress'
+    }
+
+]
