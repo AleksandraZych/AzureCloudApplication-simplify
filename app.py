964b323734c04495be54c2fa997cbfba92241552
Azych <45856462+Azych@users.noreply.github.com>
2022-06-09 18:49:47 +0200

    add mock and items in one file

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 from flask import Flask, render_template
-from mock import *
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
 	""" returns rendered homepage """
-	# items = db_helper.fetch_todo()
-	items = all_mock
-	# files = os.listdir("app/uploads")
-	#  ,, files=files
+	items = all_mock = [
+		{
+			'id': '1',
+			'task': 'Zdać',
+			'status': 'In progress'
+		},
+		{
+			'id': '2',
+			'task': 'Chlać',
+			'status': 'In progress'
+		}
+
+	]
 	return render_template("index.html", items=items)
 
+# @app.route("/delete/<int:task_id>", methods=['POST'])
+# def delete(task_id):
+# 	""" recieved post requests for entry delete """
+#
+# 	try:
+# 		db_helper.remove_task_by_id(task_id)
+# 		result = {'success': True, 'response': 'Removed task'}
+# 	except:
+# 		result = {'success': False, 'response': 'Something went wrong'}
+#
+# 	return jsonify(result)
 
 if __name__ == '__main__':
 	app.run()
